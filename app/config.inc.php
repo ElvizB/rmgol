54163aea84175d4a6e757db1386d79221ca95b4a
ElvisB <sagitario12>
2019-09-09 10:40:25 -0500

    [add] prueba

---
+++ app/config.inc.php
@@ -0,0 +1,20 @@
+<?php 
+
+define('NOMBRE_SERVIDOR', 'localhost');
+define('NOMBRE_USUARIO', 'root');
+define('PASSWORD', '');
+define('NOMBRE_BD', 'rmgol');
+
+//rutas de la web
+
+define("SERVIDOR", "http://rmgol.com/");
+define("RUTA_NOSOTROS", SERVIDOR."Nosotros");
+define("RUTA_PADULTO", SERVIDOR."programas-de-adultos");
+define("RUTA_CATEGORIA", SERVIDOR."categoria-2007");
+define("RUTA_CONTACTO", SERVIDOR."Contacto");
+define("RUTA_TOQUE", SERVIDOR."toque-final");
+//recursos
+define("RUTA_CSS", SERVIDOR . "css/");
+define("RUTA_JS", SERVIDOR . "js/");
+define("RUTA_IMG", SERVIDOR . "images/");
+
