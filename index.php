8ac109f45505ec324cf7ee843441acd5c545a115
ElvisB <sagitario12>
2019-09-09 10:43:52 -0500

    [add] prueba

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$componentes_url = parse_url($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+$componentes_url = parse_url($_SERVER['REQUEST_URI']);
 
 $ruta = $componentes_url['path'];
 
@@ -10,7 +10,7 @@ $partes_ruta = array_slice($partes_ruta, 0);
 
 $ruta_elegida = 'vistas/404.php';
 
-if($partes_ruta[0] == 'rmgol.com'){
+if($partes_ruta[0] == 'rmgol'){
   if (count($partes_ruta) == 1){
     $ruta_elegida = 'vistas/home.php';
   }else if (count($partes_ruta) == 2){
